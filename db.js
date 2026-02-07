364acd29ed250b12afed418527901395a1a7368c
Kuumaku <kimberry2547@gmail.com>
2026-02-07 14:28:14 +0700

    Second Commit

---
+++ db.js
@@ -0,0 +1,16 @@
+//Import pg in order to use database(PostgreSQL)
+//create port to connect to SQL Local server
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT),
+  database: process.env.PG_DB,
+});
+
+export default pool;
--- index.js
+++ index.js
@@ -4,3 +4,22 @@ import pool from "./db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+export const Load_name = async(req, res) => {
+    try {
+        const {results} = await pool.query(`SELECT * FROM Users_Table`);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+}
+
+app.get("/users", Load_name);
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
