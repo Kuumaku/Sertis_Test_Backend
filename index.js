f3b3e25cad0f6435acbb7af070f10f107ab5060d
Kuumaku <kimberry2547@gmail.com>
2026-02-08 08:17:06 +0700

    Commenting in logic

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+//Set up database entry in db.js
+//Store sensitive information in .env and add .env in gitignore
+//set up express cores dotenv and pg in the system
+//using pool pool to execute query
+//use gets method to get data in form of json
+//the server run on localhost 
+//Use postman to see the data that the query return
+
 import express from "express";
 import cors from "cors";
 import pool from "./db.js";
@@ -10,7 +18,8 @@ app.use(express.json());
 
 export const Load_name = async(req, res) => {
     try {
-        const {results} = await pool.query(`SELECT * FROM Users_Table`);
+        const results = await pool.query(`SELECT * FROM Users_Table`);
+        res.json(results)
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server error");
